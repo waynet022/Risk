3aa3b68e98a1878566b52eb25ff2671e61a15802
wayne1tam <waynevick7226@hotmail.com>
2020-01-03 19:40:43 -0500

    adding files

---
+++ cmake-build-debug/CMakeFiles/Risk.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Risk.dir/main.cpp.obj"
+  "Risk.exe"
+  "Risk.exe.manifest"
+  "Risk.lib"
+  "Risk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Risk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
