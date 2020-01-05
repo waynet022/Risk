73eb47422811f55a3e6f324d5ecc980accde63b0
wayne1tam <waynevick7226@hotmail.com>
2020-01-05 01:51:13 -0500

    continent update

--- Territory/Continent.cpp
+++ Territory/Continent.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "Continent.h"
+
+Continent::Continent(){
+    *continent_name_ = "unknown_continent_name";
+    continent_id_ = 0;
+    continent_bonus_army_value_ = 0;
+}
+
+Continent::Continent(string* continent_name, int continent_id, int continent_bonus_army_value){
+    continent_name_ = continent_name;
+    continent_id_ = continent_id;
+    continent_bonus_army_value_ = continent_bonus_army_value;
+}
+
+Continent::Continent(const Continent &continent){
+    continent_name_ = continent.continent_name_;
+    continent_id_ = continent.continent_id_;
+    continent_bonus_army_value_ = continent.continent_bonus_army_value_;
+}
+
+Continent::~Continent(){
+    continent_name_ = nullptr;
+    delete continent_name_;
+}
+
+string* Continent::GetContinentName(){
+    return continent_name_;
+}
+
+int Continent::GetContinentID(){
+    return continent_id_;
+}
+
+int Continent::GetContinentBonusArmyValue(){
+    return continent_bonus_army_value_;
+}
+
--- Territory/Continent.h
+++ Territory/Continent.h
@@ -5,9 +5,24 @@
 #ifndef RISK_CONTINENT_H
 #define RISK_CONTINENT_H
 
+#include <string>
+using namespace std;
 
 class Continent {
+private:
+    string* continent_name_;
+    int continent_id_;
+    int continent_bonus_army_value_;
 
+public:
+    Continent();
+    Continent(string* continent_name, int continent_id, int continent_bonus_army_value);
+    Continent(const Continent &continent);
+    ~Continent();
+
+    string* GetContinentName();
+    int GetContinentID();
+    int GetContinentBonusArmyValue();
 };
 
 
